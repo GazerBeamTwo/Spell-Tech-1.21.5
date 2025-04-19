82c5d9c2917b6cfd8081c43951f2746697ffa71c
GazerBeamTwo <thecountryboy06@gmail.com>
2025-04-19 16:42:53 +1000

    Initial commit

---
+++ src/main/java/net/gazerbeam2/spelltech/SpellTechClient.java
@@ -0,0 +1,11 @@
+package net.gazerbeam2.spelltech;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpellTechClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/gazerbeam2/spelltech/SpellTechDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gazerbeam2.spelltech;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpellTechDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
