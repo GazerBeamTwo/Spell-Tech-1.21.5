e522952caf2cc51a29f559018506b13269ccf686
GazerBeamTwo <thecountryboy06@gmail.com>
2025-04-25 09:54:04 +1000

    added spell tree button texture paths

--- src/main/java/net/gazerbeam2/spelltech/screen/SpellTreeScreen.java
+++ src/main/java/net/gazerbeam2/spelltech/screen/SpellTreeScreen.java
@@ -5,7 +5,9 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,31 +22,40 @@ public class SpellTreeScreen extends Screen {
         this.client = client;
     }
 
+
     @Override
     protected void init() {
+
+        String[] spellNames = {"Air Spell Tree", "Earth Spell Tree", "Fire Spell Tree", "Water Spell Tree",
+                "Light Spell Tree", "Dark Spell Tree", "Void  Spell Tree", "Nature  Spell Tree", "Universal  Spell Tree"};
+        Identifier[] bgTextures = {
+                Identifier.of("spelltech", "textures/gui/icons/air_spell_tree.png"),
+                Identifier.of("spelltech", "textures/gui/icons/earth_spell_tree.png"),
+                Identifier.of("spelltech", "textures/gui/icons/fire_spell_tree.png"),
+                Identifier.of("spelltech", "textures/gui/icons/water_spell_tree.png"),
+                Identifier.of("spelltech", "textures/gui/icons/light_spell_tree.png"),
+                Identifier.of("spelltech", "textures/gui/icons/dark_spell_tree.png"),
+                Identifier.of("spelltech", "textures/gui/icons/void_spell_tree.png"),
+                Identifier.of("spelltech", "textures/gui/icons/nature_spell_tree.png"),
+                Identifier.of("spelltech", "textures/gui/icons/universal_spell_tree.png"),
+        };
         // Create buttons for each spell tree
         buttons.clear();
         for (int i = 0; i < 9; i++) {
-            int index = i;
-            int y = 10 + i * 50;
+            int y = 10 + i * 42;
+
             SpellTreeWidget button = new SpellTreeWidget(
-                    -90, 0, y, 100, 40,
-                    Text.of("Spell Tree " + (index + 1)),
+                    -40, 0, y, 48, 32,
+                    Text.empty(),
                     btn -> {
-                        System.out.println("Clicked Spell Tree " + (index + 1));
-
                         SpellTreeWidget thisBtn = (SpellTreeWidget) btn;
-
                         boolean alreadyPinned = thisBtn.isPinnedOpen();
-
-                        for (SpellTreeWidget b : buttons) {
-                            b.setPinnedOpen(false);
-                        }
-
-                        // Toggle the clicked button
+                        for (SpellTreeWidget b : buttons) b.setPinnedOpen(false);
                         thisBtn.setPinnedOpen(!alreadyPinned);
-                    }
+                    },
+                    bgTextures[i]
             );
+            System.out.println("Drawing Texture: " + bgTextures[i]);
             buttons.add(button);
             addDrawableChild(button);
         }
@@ -58,16 +69,26 @@ public class SpellTreeScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+
         // Update positions based on hover
         for (SpellTreeWidget btn : buttons) {
             btn.update(mouseX, mouseY);
         }
 
-        this.renderBackground(context, mouseX, mouseY, delta); // updated to use DrawContext
+        // Draw screen background
+        this.renderBackground(context, mouseX, mouseY, delta);
+
+        // Draw title
         context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
 
-        // Let buttons draw themselves
+        // Now render widgets normally
         super.render(context, mouseX, mouseY, delta);
+
+        for (SpellTreeWidget btn : buttons) {
+            btn.renderCustom(context);
+        }
+
+
     }
 
     @Override
--- src/main/java/net/gazerbeam2/spelltech/screen/SpellTreeWidget.java
+++ src/main/java/net/gazerbeam2/spelltech/screen/SpellTreeWidget.java
@@ -1,20 +1,25 @@
 package net.gazerbeam2.spelltech.screen;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class SpellTreeWidget extends ButtonWidget {
+    private final Identifier backgroundTexture;
     private final int collapsedX;
     private final int expandedX;
     private boolean isHovered = false;
     private boolean isPinnedOpen = false;
     private int targetX;
     private int currentX;
+
     public void setPinnedOpen(boolean pinned) {
         this.isPinnedOpen = pinned;
         System.out.println("Button [" + getMessage().getString() + "] pinned: " + pinned);
@@ -24,8 +29,9 @@ public class SpellTreeWidget extends ButtonWidget {
         return isPinnedOpen;
     }
 
-    public SpellTreeWidget(int collapsedX, int expandedX, int y, int width, int height, Text message, PressAction onPress) {
+    public SpellTreeWidget(int collapsedX, int expandedX, int y, int width, int height, Text message, PressAction onPress, Identifier backgroundTexture) {
         super(collapsedX, y, width, height, message, onPress, DEFAULT_NARRATION_SUPPLIER);
+        this.backgroundTexture = backgroundTexture;
         this.collapsedX = collapsedX;
         this.expandedX = expandedX;
         this.currentX = collapsedX;
@@ -33,6 +39,17 @@ public class SpellTreeWidget extends ButtonWidget {
         this.setPosition(currentX, y);
     }
 
+    public void renderCustom(DrawContext context) {
+        context.drawTexture(
+                RenderLayer::getGuiTextured,
+                this.backgroundTexture,
+                this.getX(), this.getY(),
+                0f, 0f,
+                this.getWidth(), this.getHeight(),
+                this.getWidth(), this.getHeight(),
+                0xFFFFFFFF
+        );
+    }
 
     public void update(int mouseX, int mouseY) {
         // First, apply current position
